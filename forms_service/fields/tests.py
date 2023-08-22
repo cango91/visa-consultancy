4a5051e389dbbb8d1052ad1639f72676b3eb69eb
cango91 <can.gologlu@gmail.com>
2023-08-22 15:44:04 -0400

    refactor tests. Add TextField tests

---
+++ forms_service/fields/tests.py
@@ -0,0 +1,25 @@
+from django.test import TestCase
+from .text_field import TextField
+from forms_service.validators.validators import MinLengthValidator, MaxLengthValidator
+
+class TextFieldTests(TestCase):
+    def test_init_with_valid_values_single_arg(self):
+        field = TextField('Your Name')
+        self.assertIsNotNone(field)
+        
+    def test_init_invalid(self):
+        with self.assertRaises(TypeError):
+            TextField()
+    
+    def test_init_with_validators(self):
+        field = TextField('Your name',validations=[MaxLengthValidator(128),MinLengthValidator(3)])
+        self.assertIsNotNone(field)
+        self.assertTrue(field.enabled)
+        
+    def test_serialization(self):
+        serialized = TextField('Your name',validations=[MaxLengthValidator(128),MinLengthValidator(3)]).serialize()
+        self.assertTrue(serialized['label']=='Your name')
+        self.assertTrue(serialized['enabled']==True)
+        self.assertTrue(isinstance(serialized['validations'],list))
+        self.assertTrue(serialized['type']=='text')
+        
---
+++ forms_service/validators/tests.py
@@ -0,0 +1,64 @@
+
+from django.test import TestCase
+from forms_service.validators.base_validator import ArgumentValidationError, BaseValidator
+from forms_service.validators.constraints.min_value import MinValueConstraint
+from forms_service.validators.validators import MinLengthValidator, MaxLengthValidator,ValidationError
+
+
+class ConcreteValidator(BaseValidator):
+    def get_expected_args(self):
+        return [(int, [MinValueConstraint(5)])]
+
+    def validate(self, value):
+        pass
+
+## BaseValidator tests ##
+class TestBaseValidator(TestCase):
+    def test_init_with_correct_args(self):
+        validator = ConcreteValidator(10)
+        self.assertEqual(validator.args, (10,))
+
+    def test_init_with_incorrect_type(self):
+        with self.assertRaises(TypeError):
+            ConcreteValidator("string")
+
+    def test_init_with_constraint_violation(self):
+        with self.assertRaises(ArgumentValidationError):
+            ConcreteValidator(3)
+
+    def test_init_with_missing_args(self):
+        with self.assertRaises(ValueError):
+            ConcreteValidator()
+            
+    def test_serialization(self):
+        serialized = ConcreteValidator(5).serialize()
+        self.assertTrue(serialized['validator'] == 'ConcreteValidator')
+
+## Min/MaxLengthValidator tests ##
+class TestLengthValidators(TestCase):
+    def test_min_length_validator_valid(self):
+        validator = MinLengthValidator(5)
+        self.assertIsNone(validator.validate('Hello'))
+
+    def test_min_length_validator_invalid(self):
+        validator = MinLengthValidator(10)
+        with self.assertRaises(ValidationError):
+            validator.validate('Short')
+
+    def test_max_length_validator_valid(self):
+        validator = MaxLengthValidator(5)
+        self.assertIsNone(validator.validate('Hello'))
+
+
+    def test_max_length_validator_invalid(self):
+        validator = MaxLengthValidator(3)
+        with self.assertRaises(ValidationError):
+            validator.validate('Too Long')
+            
+    def test_serialization(self):
+        serialized_min = MinLengthValidator(5).serialize()
+        serialized_max = MaxLengthValidator(5).serialize()
+        self.assertTrue(serialized_min['arguments']==[5])
+        self.assertTrue(serialized_max['arguments']==[5])
+        self.assertTrue(serialized_min['validator'] == 'MinLengthValidator')
+        self.assertTrue(serialized_max['validator'] == 'MaxLengthValidator')
